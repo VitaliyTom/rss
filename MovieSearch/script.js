48c2a661a81b53a90f152b44c6791fdcab87a9dc
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-05-03 02:05:56 +0300

    fix: implement correct loading data from OMDb

--- MovieSearch/script.js
+++ MovieSearch/script.js
@@ -3,34 +3,76 @@ import Card from "./card.js";
 const form = document.querySelector(".form");
 const key = "2ca9f536";
 
-form.addEventListener("submit", (e) => {
-  const fieldInput = document.querySelector(".field_input").value.toString();
-  let url = `https://www.omdbapi.com/?s=${fieldInput}&apikey=${key}`;
-  let cards = [];
-  let rating = [];
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  searchFilm(rating, cards, url);
-  
+
+  const fieldInput = document.querySelector(".field_input").value.toString();
+  const url = `https://www.omdbapi.com/?s=${fieldInput}&apikey=${key}`;
+
+  const cards = await searchFilm(url);
+  console.log("+++", cards);
+
+  addElement(cards);
+  form.reset();
 });
 
-async function searchFilm(rating, cards, url) {
-  let response = await fetch(url);
-  let parsed = await response.json();
-  console.log(parsed);
+function addElement(cards) {
+  const cardView = document.querySelector(".card_view");
+  let result = "";
+  console.log("-+", cards);
+
+  cards.forEach((el) => {
+    console.log(el);
+
+    result += `<div class="card">
+                  <div class="name_card">
+                      <p>${el.title}</p>
+                  </div>
+                  <div class="poster">
+                      <img src="${el.poster}" alt="poster">
+                  </div>
+                  <div class="release_year">
+                      <p>${el.year}</p>
+                  </div>
+                  <div class="rating">
+                      <div class="icon_rating">
+                        <img src="img/star2.png" alt="icon">
+                      </div>
+                      <div class="rating_score">
+                          <p>${el.rating.slice(0, 3)}</p>
+                      </div>
+                  </div>
+               </div>           
+      `;
+  });
+  console.log(result);
+  cardView.innerHTML = result;
+}
+
+async function searchFilm(url) {
+  const rating = [];
+  const response = await fetch(url);
+  const parsed = await response.json();
+  console.log("parsed = ", parsed);
   parsed.Search.forEach((el) => {
     rating.push(el.imdbID);
   });
+  console.log("rating", rating);
 
-  searchRating(rating, cards);
+  const result = searchRating(rating);
+
+  return result;
 }
 
-function searchRating(rating, cards) {
-  rating.forEach(async (el) => {
-    let response = await fetch(
+async function searchRating(rating) {
+  const cards = [];
+
+  const cardPromises = rating.map(async (el) => {
+    const response = await fetch(
       `https://www.omdbapi.com/?i=${el}&apikey=${key}`
     );
 
-    let parsed = await response.json();
+    const parsed = await response.json();
 
     cards.push(
       new Card(
@@ -41,6 +83,9 @@ function searchRating(rating, cards) {
       )
     );
   });
-  console.log(cards);
-  form.reset();
+
+  await Promise.all(cardPromises);
+  console.log("----", cards);
+
+  return cards;
 }
