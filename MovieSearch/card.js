065507924f281f41c2e522a57470e270bc19987b
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-05-10 23:28:52 +0300

    refactor: moved card.js into js/card.js

--- MovieSearch/card.js
+++
@@ -1,8 +0,0 @@
-function Card(poster, title, year, rating) {
-  this.poster = poster;
-  this.title = title;
-  this.year = year;
-  this.rating = rating;
-}
-
-export default Card;
---
+++ MovieSearch/js/card.js
@@ -0,0 +1,9 @@
+function Card(poster, title, year, rating, id) {
+	this.poster = poster;
+	this.title = title;
+	this.year = year;
+	this.rating = rating;
+	this.id = id;
+}
+
+export default Card;
