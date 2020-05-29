5d4349496f2349717e7f2e7d4749e2f676c6aa51
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-05-29 15:17:13 +0300

    feat: add object day

---
+++ fancy-weather/src/general/day.js
@@ -0,0 +1,17 @@
+const dayName = {
+	0: 'Sunday',
+	1: 'Monday',
+	2: 'Tuesday',
+	3: 'Wednesday',
+	4: 'Thursday',
+	5: 'Friday',
+	6: 'Saturday'
+};
+
+export default dayName;
+// Понедельник
+// Вторник
+// Среда
+// Четверг
+// Пятница
+// Суббота
