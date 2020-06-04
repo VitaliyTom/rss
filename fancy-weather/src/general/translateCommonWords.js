93620b74a256d624ac6d1f94426b6ee48822eb57
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-06-04 11:24:06 +0300

    feat: add translation of common words

---
+++ fancy-weather/src/general/translateCommonWords.js
@@ -0,0 +1,11 @@
+export const translateCommonWords = {
+	search: [ 'Search city or ZIP', 'Поиск города или индекс', 'Пошук горада ці індэкс' ],
+	searchBtn: [ 'Search', 'Поиск', 'Пошук' ],
+	feelslike: [ 'feelslike', 'ощущается', 'адчуваецца' ],
+	wind: [ 'wind', 'ветер', 'вецер' ],
+	humidity: [ 'humidity', 'влажность', 'вільготнасць' ],
+	windUnits: [ 'm/s', 'м/с', 'м/с' ],
+	latitude: [ 'Latitude', 'Широта', 'Шырата' ],
+	longitude: [ 'Longitude', 'Долгота', 'Даўгата' ],
+	error: [ 'There is no such city', 'Такого города не существует', 'Такога горада не існуе' ]
+};
