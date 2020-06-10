0ab6846a0a61b579aa58a51933a2a6eae4a0a7ec
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-06-10 13:56:01 +0300

    refactor: complete changed editing DOM elements

--- fancy-weather/src/weather_today/addElementWeather.js
+++ fancy-weather/src/weather_today/addElementWeather.js
@@ -1,90 +1,94 @@
 import { getLanguage } from '../general/lng.js';
 import { translateCommonWords } from '../general/translateCommonWords.js';
 import { weatherConditions } from '../general/weatherConditions.js';
+import { dayNameAbbreviat } from '../general/day.js';
+import { monthName } from '../general/month.js';
 
 function localDate() {
 	const locDate = document.querySelector('.date');
 	setInterval(() => {
-		const date = new Date();
 		const options = {
-			month: 'long',
-			weekday: 'short',
-			day: 'numeric',
-			timezone: 'UTC',
 			hour: 'numeric',
 			minute: 'numeric',
 			second: 'numeric'
 		};
 
-		let setHtmlDate = date.toLocaleString('ru', options);
-		localStorage.getItem('lang') === 'Eng' && (setHtmlDate = date.toLocaleString('en-US', options));
+		let nowDate = new Date().getTime();
+		let date = new Date(1970, 0, 1, 0, 0, localStorage.getItem('timezone'), nowDate);
+
+		const day = date.getDay();
+		const month = date.getMonth();
+		const monthDay = date.getDate();
+		const setHtmlDate = `${getNameDay(day)} ${monthDay} ${getNameMonth(month)} ${localStorage.getItem('lang') ===
+		'Eng'
+			? date.toLocaleString('en-US', options)
+			: date.toLocaleString('ru', options)}`;
+
 		locDate.innerText = `${setHtmlDate}`;
 	}, 1000);
 }
 
 function addElementWeather(weatherObj, regionCity) {
-	const weatherToday = document.querySelector('.weather_today');
-	const region = document.querySelector('.region');
 	const searchInput = document.querySelector('.search_input');
 	const searchBtn = document.querySelector('.search_btn');
-
-	let result = '';
-	result = `
-    <div class="region">
-        <p>${regionCity.country}, ${regionCity.city} ${regionCity.residential === undefined || 'undefined'
-		? ''
-		: regionCity.residential === undefined || 'undefined' ? '' : regionCity.residential}</p>
-        <p class="date"></p>
-    </div>
-    <div class="container_weather">
-        <div class="weather_temperature">
-            <div class="temperature">
-                <p>${Math.round(weatherObj.temp.value)}°</p>
-            </div>            
-        </div>
-        <div class="info">
-            <div class="temperature_img">
-                <img src="src/img/${weatherObj.weather_code}.svg" alt="${weatherObj.weather_code}">
-            </div>
-            <ul>
-                <li>${weatherConditions[weatherObj.weather_code][getLanguage()]}</li>
-                <li>${translateCommonWords.feelslike[getLanguage()]}: ${weatherObj.feelslike.value}°</li>
-                <li>${translateCommonWords.wind[getLanguage()]}: ${weatherObj.wind.value} ${translateCommonWords
-		.windUnits[getLanguage()]}</li>
-                <li>${translateCommonWords.humidity[getLanguage()]}: ${weatherObj.humidity.value}%</li>
-            </ul>
-        </div>
-    </div>`;
-
-	if (region) {
-		Array.from(weatherToday.children).forEach((element) => {
-			element.remove();
-		});
-
-		document.querySelector('.weather_three_day').remove();
-	}
-
-	weatherToday.insertAdjacentHTML('afterbegin', result);
-
-	const wrapperMap = document.querySelector('.wrapper_map');
-	const coordinates = document.querySelector('.coordinates');
-
-	if (coordinates) {
-		coordinates.remove();
-	}
-
-	wrapperMap.insertAdjacentHTML(
-		'beforeEnd',
-		`<div class="coordinates"><div class="lat">${translateCommonWords.latitude[
-			getLanguage()
-		]}: ${localStorage.getItem('DMS_lat')}</div>
-	<div class="lon">${translateCommonWords.longitude[getLanguage()]}: ${localStorage.getItem('DMS_lon')}</div> </div>`
-	);
+	const cityName = document.querySelector('.city_name');
+	const tempValue = document.querySelector('.temp_value');
+	const tempImg = document.querySelector('.temp_img');
+	const weatherCondition = document.querySelector('.weather_conditions');
+	const weatherFeelslike = document.querySelector('.weather_feelslike');
+	const weatherWind = document.querySelector('.weather_wind');
+	const weather_humidity = document.querySelector('.weather_humidity');
+	const coordinatesLat = document.querySelector('.lat');
+	const coordinatesLon = document.querySelector('.lon');
 
 	localDate();
+
 	searchInput.setAttribute('placeholder', translateCommonWords.search[getLanguage()]);
 	searchInput.classList.remove('active');
 	searchBtn.innerText = `${translateCommonWords.searchBtn[getLanguage()]}`;
+
+	cityName.innerHTML = `${regionCity.country}, ${regionCity.city} 
+	
+	
+	${regionCity.residential === undefined || regionCity.residential === 'undefined' ? '' : regionCity.residential}`;
+
+	tempValue.innerHTML = `${weatherObj.temp.value === null
+		? Math.round(localStorage.getItem('averageTemp'))
+		: Math.round(weatherObj.temp.value)}°`;
+
+	tempImg.setAttribute(
+		'src',
+		`src/img/${weatherObj.weather_code === null
+			? localStorage.getItem('weatherCode')
+			: weatherObj.weather_code}.svg`
+	);
+	tempImg.setAttribute('alt', `${weatherObj.weather_code}`);
+	weatherCondition.innerText = `${weatherConditions[
+		weatherObj.weather_code === null ? localStorage.getItem('weatherCode') : weatherObj.weather_code
+	][getLanguage()]}`;
+	weatherFeelslike.innerText = `${translateCommonWords.feelslike[getLanguage()]}: ${Math.round(
+		weatherObj.feelslike.value
+	)}°`;
+	weatherWind.innerText = `${translateCommonWords.wind[getLanguage()]}: ${Math.round(
+		weatherObj.wind.value
+	)} ${translateCommonWords.windUnits[getLanguage()]}`;
+	weather_humidity.innerText = `${translateCommonWords.humidity[getLanguage()]}: ${Math.round(
+		weatherObj.humidity.value
+	)}%`;
+
+	coordinatesLat.innerText = `${translateCommonWords.latitude[getLanguage()]}: ${localStorage.getItem('DMS_lat')}`;
+	coordinatesLon.innerText = `${translateCommonWords.longitude[getLanguage()]}: ${localStorage.getItem('DMS_lon')}`;
+
+	const backGroundBar = document.querySelector('.back_ground');
+	backGroundBar.classList.remove('active');
+}
+
+function getNameDay(params) {
+	return dayNameAbbreviat[params][getLanguage()];
+}
+
+function getNameMonth(params) {
+	return monthName[params][getLanguage()];
 }
 
 export default addElementWeather;
