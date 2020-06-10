0753efd159523f2b604f4afb1d90f88f0fcec1c5
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-06-10 13:47:47 +0300

    refactor: apply prettier

--- fancy-weather/src/geolocation/regionObj.js
+++ fancy-weather/src/geolocation/regionObj.js
@@ -1,4 +1,15 @@
-function RegionObj(ISOCountry, city, city_district, continent, country, country_code, postcode, residential, suburb) {
+function RegionObj(
+	ISOCountry,
+	city,
+	city_district,
+	continent,
+	country,
+	country_code,
+	postcode,
+	residential,
+	suburb,
+	timezone
+) {
 	this.ISOCountry = ISOCountry;
 	this.city = city;
 	this.city_district = city_district;
@@ -8,6 +19,7 @@ function RegionObj(ISOCountry, city, city_district, continent, country, country_
 	this.postcode = postcode;
 	this.residential = residential;
 	this.suburb = suburb;
+	this.timezone = timezone;
 }
 
 export default RegionObj;
