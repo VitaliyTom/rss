a68280a54c5b034c75170894ef8582633fca4648
VitaliyTom <39959686+VitaliyTom@users.noreply.github.com>
2020-08-19 12:07:28 +0300

    fix: replace the first letter (#12)

--- fancy-weather/src/general/month.js
+++ fancy-weather/src/general/month.js
@@ -1,14 +1,14 @@
 export const monthName = {
-	0: [ 'January', 'Января', 'Студзеня', 'styczeń' ],
-	1: [ 'February', 'Февраля', 'Лютага', 'luty' ],
-	2: [ 'March', 'Марта', 'Сакавiка', 'marzec' ],
-	3: [ 'April', 'Апреля', 'Красавiка', 'kwiecień' ],
-	4: [ 'May', 'Мая', 'Мая', 'maj' ],
-	5: [ 'June', 'Июня', 'Червеня', 'czerwiec' ],
-	6: [ 'July', 'Июля', 'Лiпеня', 'lipiec' ],
-	7: [ 'August', 'Августа', 'Жніўня', 'sierpień' ],
-	8: [ 'September', 'Сентября', 'Верасня', 'wrzesień' ],
-	9: [ 'October', 'Октября', 'Кастрычнiка', 'październik' ],
-	10: [ 'November', 'Ноября', 'Лiстапада', 'listopad' ],
-	11: [ 'December', 'Декабря', 'Снежня', 'grudzień' ]
+	0: [ 'January', 'Января', 'Студзеня', 'Styczeń' ],
+	1: [ 'February', 'Февраля', 'Лютага', 'Luty' ],
+	2: [ 'March', 'Марта', 'Сакавiка', 'Marzec' ],
+	3: [ 'April', 'Апреля', 'Красавiка', 'Kwiecień' ],
+	4: [ 'May', 'Мая', 'Мая', 'Maj' ],
+	5: [ 'June', 'Июня', 'Червеня', 'Czerwiec' ],
+	6: [ 'July', 'Июля', 'Лiпеня', 'Lipiec' ],
+	7: [ 'August', 'Августа', 'Жніўня', 'Sierpień' ],
+	8: [ 'September', 'Сентября', 'Верасня', 'Wrzesień' ],
+	9: [ 'October', 'Октября', 'Кастрычнiка', 'Październik' ],
+	10: [ 'November', 'Ноября', 'Лiстапада', 'Listopad' ],
+	11: [ 'December', 'Декабря', 'Снежня', 'Grudzień' ]
 };
